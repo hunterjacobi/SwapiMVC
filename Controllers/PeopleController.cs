a2712415ccc536e42a6135791f4009d7b9a45746
Hunter Browning <guccigod@Hunters-MacBook-Pro.local>
2022-04-12 10:46:23 -0400

    Injecting into People Controller

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,11 @@ namespace SwapiMVC.Controllers
 {
     public class PeopleController : Controller
     {
+        private readonly HttpClient _httpClient;
+        public PeopleController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("swapi");
+        }
         public IActionResult Index()
         {
             return View();
